f291b42bc40bca0b181a68ce201aced38d7a6348
MexL <madiajijah7@gmail.com>
2019-12-21 14:52:10 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #3490dc; /*#fff*/
+                color: White; /*#636b6f*/
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -49,7 +49,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: White; /*#636b6f*/
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
