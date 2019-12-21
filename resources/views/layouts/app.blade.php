97936e9ef5f50beb2dc1d05de6d8ce51f9cec26b
MexL <madiajijah7@gmail.com>
2019-12-21 14:37:06 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,14 @@
     <title>{{ config('app.name', 'Data Barang') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('theme/dist/js/bootstrap.bundle.js') }}" defer></script>
-    <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('theme/dist/css/bootstrap.css') }}">
-    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
